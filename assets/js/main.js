8cb6ef791b897bc9d2f10416cdeb1511d03064ee
ooodhctvorec <ooo.dhc.tvorec@gmail.com>
2026-05-02 19:31:26 +0300

    Replace ceramic studios label

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,11 @@
 
+const siteLabel = 'Студии керамики в вашей ОО под ключ';
+const textWalker = document.createTreeWalker(document.body, NodeFilter.SHOW_TEXT);
+let textNode;
+while ((textNode = textWalker.nextNode())) {
+  textNode.nodeValue = textNode.nodeValue.replaceAll('Керамические студии', siteLabel);
+}
+
 document.querySelectorAll('.menu-toggle').forEach(btn=>{btn.addEventListener('click',()=>document.querySelector('.main-nav')?.classList.toggle('open'))});
 document.querySelectorAll('.slider').forEach(slider=>{const track=slider.querySelector('.slider-track');slider.querySelector('.prev')?.addEventListener('click',()=>track.scrollBy({left:-track.clientWidth*.85,behavior:'smooth'}));slider.querySelector('.next')?.addEventListener('click',()=>track.scrollBy({left:track.clientWidth*.85,behavior:'smooth'}));});
 document.querySelectorAll('img').forEach(image=>{image.addEventListener('error',()=>{image.classList.add('image-missing');image.removeAttribute('src');});});
